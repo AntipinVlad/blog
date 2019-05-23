b83fd536e67408d8aeb0d5703d21242e739a49dc
AntipinVlad <32509214+AntipinVlad@users.noreply.github.com>
2019-05-23 19:29:05 +0300

    Post request

--- src/components/create.components.js
+++ src/components/create.components.js
@@ -1,6 +1,7 @@
 import { Component } from '../core/component'
 import { Form } from '../core/form'
 import { Validators } from '../core/validators'
+import { apiService } from '../services/api.service'
 
 export class CreateComponent extends Component {
     constructor(id) {
@@ -10,20 +11,26 @@ export class CreateComponent extends Component {
     init() {
         this.$el.addEventListener('submit', submitHandler.bind(this));
 
-        this.from = new Form(this.$el, {
+        this.form = new Form(this.$el, {
             title: [Validators.required],
-            fulltext: [Validators.required]
+            fulltext: [Validators.required, Validators.minLenght(10)]
         });
     }
 }
 
-function submitHandler(event) {
+async function submitHandler(event) {
     event.preventDefault()
+    // Проверка на валидность
+    if (this.form.isValid()) {
+        // Получение значений формы
+        const formData = {
+            type: this.$el.type.value,
+            date: new Date().toLocaleDateString(),
+            ...this.form.value()
+        }
+        await apiService.createPost(formData);
+        this.form.clear();
+        alert('Запись создана в БД');
 
-    const formData = {
-        type: this.$el.type.value,
-        ...this.from.value()
     }
-    console.log('Submit', formData)
-
 }
--- src/core/validators.js
+++ src/core/validators.js
@@ -1,6 +1,11 @@
 // Валидатор проверяет пустая ли строка
 export class Validators {
-    static required(value = ''){
+    static required(value = '') {
         return value && value.trim();
+    }
+    static minLenght(length) {
+        return value =>{
+            return value.length >= length;
+        }
     }
 }
